18f24bfd80ff61cb91dfd2f6790b92246267c8e8
ariel <arieldiaz05@gmail.com>
2023-08-30 10:53:03 +0200

    Commit inicial

---
+++ src/main/java/com/example/demoroles/DemoRolesApplication.java
@@ -0,0 +1,12 @@
+package com.example.demoroles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRolesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoRolesApplication.class, args);
+    }
+}

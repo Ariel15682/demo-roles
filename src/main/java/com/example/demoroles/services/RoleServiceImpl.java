773d3e58ccabd0878e5d418a4efd4900eee2e24a
ariel <arieldiaz05@gmail.com>
2023-08-30 11:20:31 +0200

    Creacion clase de implementacion de interfaz RoleService, RoleServiceImpl

---
+++ src/main/java/com/example/demoroles/services/RoleServiceImpl.java
@@ -0,0 +1,18 @@
+package com.example.demoroles.services;
+
+import com.example.demoroles.entities.Role;
+import com.example.demoroles.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service(value = "roleService")
+public class RoleServiceImpl implements RoleService {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public Role findByName(String name) {
+        return roleRepository.findRoleByName(name);
+    }
+}

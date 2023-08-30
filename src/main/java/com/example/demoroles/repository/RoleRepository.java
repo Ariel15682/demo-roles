d444874f53f77f3eb9f7245bcb5dd0a4002539c8
ariel <arieldiaz05@gmail.com>
2023-08-30 11:13:54 +0200

    Creacion Interfaz RoleRepository

---
+++ src/main/java/com/example/demoroles/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.example.demoroles.repository;
+
+import com.example.demoroles.entities.Role;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Long> {
+    Role findRoleByName(String name);
+}
